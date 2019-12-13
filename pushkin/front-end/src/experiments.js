3b7c72e49a085ecfcf18531bef0fdaf83e37510a
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2019-12-13 10:22:29 -0500

    Stripped out more custom stuff. Made sure the tempPackages folders are there.

--- pushkin/front-end/src/experiments.js
+++ pushkin/front-end/src/experiments.js
@@ -1,11 +1,1 @@
-import pushkinComponent7f3393db49634321ad9304c90b516476 from 'pushkinComponent7f3393db49634321ad9304c90b516476';
-export default [
-  {
-    fullName: 'newexp Experiment',
-    shortName: 'newexp',
-    module: pushkinComponent7f3393db49634321ad9304c90b516476,
-    logo: 'logo512.png',
-    tagline: 'Be a citizen scientist! Try this quiz.',
-    duration: ''
-  }
-];
+export default [];
