0ef73e6548e10994e02bd0c1e8986bda670605ef
hrcn <hrcn1128@gmail.com>
2020-05-06 02:30:50 -0400

    remove serviceWorker

--- pushkin/front-end/src/index.js
+++ pushkin/front-end/src/index.js
@@ -22,7 +22,6 @@ import rootSaga from './sagas/index';
 
 //utilities
 //import history from './utils/history';
-import * as serviceWorker from './serviceWorker';
 import { Auth0Provider } from './utils/react-auth0-spa';
 import App from './App';
 import { CONFIG } from './config';
@@ -60,8 +59,3 @@ render(
   </Auth0Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
