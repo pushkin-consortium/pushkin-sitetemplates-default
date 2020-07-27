b9a6de1d9bd81715629fe633aa16433a0e7efc3a
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-07-27 11:43:32 -0400

    Fixed bugs affecting api controller mounting.

--- pushkin/api/src/index.js
+++ pushkin/api/src/index.js
@@ -12,8 +12,11 @@ api.init()
 		// load in user controllers
 		const controllersFile = path.join(__dirname, 'controllers.json');
 		const controllers = JSON.parse(fs.readFileSync(controllersFile));
-		Object.keys(controllers).forEach(contrModule => {
-			api.usePushkinController(controllers[contrModule], contrModule); //Nests the Express router app for this experiment at the route /api/[exp], where [exp] is the path for the experiment in question.
+		Object.keys(controllers).forEach(controller => {
+			const mountPath = path.join('/api/', controllers[controller]);
+			const contrModule = require(controller);
+			console.log("Mounting ", controller);
+			api.usePushkinController(mountPath, contrModule); //Nests the Express router app for this experiment at the route /api/[exp], where [exp] is the path for the experiment in question.
 		});
 		api.start();
 	})
