fb70c065b9c5cba72e25520ab3e04e4f360afc6f
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-08-05 18:54:37 -0400

    Updated docker start for ECS compatibility with AMPQ. Also added DockerHubID to pushkin.yaml

--- pushkin/api/dockerStart.sh
+++ pushkin/api/dockerStart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "API container loaded, waiting for rabbitmq"
-while ! nc -z message-queue 5672; do sleep 3; done
+AMPQ_DOMAIN=$(echo $AMPQ_ADDRESS | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")
+echo "Waiting for port 5672 on $AMPQ_DOMAIN"
+while ! nc -z $AMPQ_DOMAIN 5672; do sleep 3; done
 echo "API container found rabbitmq, starting"
 
 yarn start
