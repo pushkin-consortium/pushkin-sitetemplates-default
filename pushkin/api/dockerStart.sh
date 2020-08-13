3731a64e53415c420e66ceb3f7db5174e19bb50d
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-08-13 09:44:57 -0400

    Fixed typo in dockerStart

--- pushkin/api/dockerStart.sh
+++ pushkin/api/dockerStart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "API container loaded, waiting for rabbitmq"
-AMPQ_DOMAIN=$(echo $AMPQ_ADDRESS | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")
-echo "Waiting for port 5672 on $AMPQ_DOMAIN"
-while ! nc -z $AMPQ_DOMAIN 5672; do sleep 3; done
+AMQP_DOMAIN=$(echo $AMQP_ADDRESS | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")
+echo "Waiting for port 5672 on $AMQP_DOMAIN"
+while ! nc -z $AMQP_DOMAIN 5672; do sleep 3; done
 echo "API container found rabbitmq, starting"
 
 yarn start
