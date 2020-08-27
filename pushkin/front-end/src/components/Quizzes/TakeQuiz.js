2370cb8bf60e85aab0ffa53f5b84a23d8b0c7649
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2020-08-27 11:09:24 -0400

    Finished migration to new CLI

--- pushkin/front-end/src/components/Quizzes/TakeQuiz.js
+++ pushkin/front-end/src/components/Quizzes/TakeQuiz.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import experiments from '../../experiments.js';
 import { connect } from 'react-redux';
-import CONFIG from '../../config';
+import { CONFIG } from '../../config';
 
 const expObject = {};
 experiments.forEach((exp) => {
@@ -19,9 +19,13 @@ class TakeQuiz extends React.Component {
   render() {
     const { match } = this.props;
     const QuizComponent = expObject[match.params.quizName];
+    console.log(CONFIG.apiEndpoint.concat(match.params.quizName)); //FUBAR
     return (
       <div>
-        <QuizComponent {...this.props} api={CONFIG.apiEndpoint} />
+        <QuizComponent
+          {...this.props}
+          api={CONFIG.apiEndpoint.concat('/').concat(match.params.quizName)}
+        />
       </div>
     );
   }
